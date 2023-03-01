9b7ea35af3ecf0969d4319d5acbb85a20de50b1a
Julieta Miguel <julietamiguel@hotmail.com>
2023-02-28 23:18:28 -0300

    My first Commit

---
+++ Sintaxis.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById('boton').onclick = function mostrar () {
+    document.getElementById('info_personal').style.display= 'block';
+}
+
+document.getElementById('ocultar').onclick = function ocultar () {
+    document.getElementById('info_personal').style.display= 'none';
+}
